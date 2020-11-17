672938cfca7e5f47f9bb29fb702c93fb639453a7
jh64 <48374211+jh64@users.noreply.github.com>
2020-11-17 22:59:03 +1100

    add the spring batch job and logic to process the order - to be tested.

--- src/main/java/com/mycompany/myapp/repository/WaterOrderRepository.java
+++ src/main/java/com/mycompany/myapp/repository/WaterOrderRepository.java
@@ -17,6 +17,6 @@ public interface WaterOrderRepository extends JpaRepository<WaterOrder, Long> {
     @Query("select waterOrder from WaterOrder waterOrder where waterOrder.user.login = ?#{principal.username}")
     List<WaterOrder> findByUserIsCurrentUser();
     
-    @Query("select waterOrder from WaterOrder waterOrder where waterOrder.status = ?'INPROGRESS'")
+    @Query("select waterOrder from WaterOrder waterOrder where waterOrder.status = 'INPROGRESS'")
     List<WaterOrder> findAllInprogress();
 }
